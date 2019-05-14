38a57e922524180127dcd9eb869f6c5e91687da7
mauribelu <mauribelu@unal.edu.co>
2019-05-14 18:47:05 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,51 +1,20 @@
-#lectura de los archivos
-import glob
+#Cambiar , por . en decimales
+sed 's/\,/./g' estacion1.csv > estacion1a.csv
+sed 's/\,/./g' estacion2.csv > estacion2a.csv
+sed 's/\,/./g' estacion3.csv > estacion3a.csv
+sed 's/\,/./g' estacion4.csv > estacion4a.csv
+# Cambiar delimitador ; por ,
+sed 's/\;/,/g' estacion1a.csv > estacion1b.csv
+sed 's/\;/,/g' estacion2a.csv > estacion2b.csv
+sed 's/\;/,/g' estacion3a.csv > estacion3b.csv
+sed 's/\;/,/g' estacion4a.csv > estacion4b.csv
+#Agregar Nombre estación para cada uno de los archivos
+sed 's|\([0-9][0-9]\)\/\([0-9][0-9]\)\/\([0-9][0-9]\)|estacion1,\1\2\3|g' estacion1b.csv > estacion1c.csv
+sed 's|\([0-9][0-9]\)\/\([0-9][0-9]\)\/\([0-9][0-9]\)|estacion2,\1\2\3|g' estacion2b.csv > estacion2c.csv
+sed 's|\([0-9][0-9]\)\/\([0-9][0-9]\)\/\([0-9][0-9]\)|estacion3,\1\2\3|g' estacion3b.csv > estacion3c.csv
+sed 's|\([0-9][0-9]\)\/\([0-9][0-9]\)\/\([0-9][0-9]\)|estacion4,\1\2\3|g' estacion4b.csv > estacion4c.csv
+#unir archivos 
+cat estacion1c.csv estacion2c.csv estacion3c.csv estacion4c.csv > estacion1234.csv
 
-filenames = glob.glob('lab-02/*.csv')
-filenames
-
-#se leen cada uno de los archivos para ver su estructura
-text1 = []
-for filename in filenames:
-    with open('lab-02/estacion1.csv', 'rt') as f1:
-        text1 = f1.readlines()
-text1 [:5]
-
-text2 = []
-for filename in filenames:
-    with open('lab-02/estacion2.csv', 'rt') as f2:
-        text2 = f2.readlines()
-text2 [:5]
-
-text3 = []
-for filename in filenames:
-    with open('lab-02/estacion3.csv', 'rt') as f3:
-        text3 = f3.readlines()
-text3 [:5]
-
-text4 = []
-for filename in filenames:
-    with open('lab-02/estacion4.csv', 'rt') as f4:
-        text4 = f4.readlines()
-text4 [:5]
-
-#Decimales 
-text1 = [line.replace(',', '.') for line in text1]
-text2 = [line.replace(',', '.') for line in text2]
-text3 = [line.replace(',', '.') for line in text3]
-text4 = [line.replace(',', '.') for line in text4]
-
-#se elimina el retorno de carro '\n'
-text1 = [line[:-1] for line in text1]
-text2 = [line[:-1] for line in text2]
-text3 = [line[:-1] for line in text3]
-text4 = [line[:-1] for line in text4]
-
-
-## Se separan los campos deliminados por comas
-text1 = [line.replace(';', ',') for line in text1]
-text2 = [line.replace(';', ',') for line in text2]
-text3 = [line.replace(';', ',') for line in text3]
-text4 = [line.replace(';', ',') for line in text4]
-
-
+#se instala libreria para consultas sql
+sudo apt-get install -y csvkit
