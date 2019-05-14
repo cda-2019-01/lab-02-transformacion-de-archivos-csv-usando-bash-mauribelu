742eded7b19625b6060821398bbfbf50058c94fa
mauribelu <mauribelu@unal.edu.co>
2019-05-13 23:17:32 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,51 @@
-# reemplazo de , por . y ; por , estacion1.csv
-sed 's/\,/./g' estacion1.csv > estacion1a.csv
-sed 's/\;/,/g' estacion1a.csv > estacion1b.csv
-# reemplazo de , por . y ; por , estacion2.csv
-sed 's/\,/./g' estacion2.csv > estacion2a.csv
-sed 's/\;/,/g' estacion2a.csv > estacion2b.csv
-# reemplazo de , por . y ; por , estacion3.csv
-sed 's/\,/./g' estacion3.csv > estacion3a.csv
-sed 's/\;/,/g' estacion3a.csv > estacion3b.csv
-# reemplazo de , por . y ; por , estacion3.csv
-sed 's/\,/./g' estacion4.csv > estacion3a.csv
-sed 's/\;/,/g' estacion4a.csv > estacion3b.csv
+#lectura de los archivos
+import glob
+
+filenames = glob.glob('lab-02/*.csv')
+filenames
+
+#se leen cada uno de los archivos para ver su estructura
+text1 = []
+for filename in filenames:
+    with open('lab-02/estacion1.csv', 'rt') as f1:
+        text1 = f1.readlines()
+text1 [:5]
+
+text2 = []
+for filename in filenames:
+    with open('lab-02/estacion2.csv', 'rt') as f2:
+        text2 = f2.readlines()
+text2 [:5]
+
+text3 = []
+for filename in filenames:
+    with open('lab-02/estacion3.csv', 'rt') as f3:
+        text3 = f3.readlines()
+text3 [:5]
+
+text4 = []
+for filename in filenames:
+    with open('lab-02/estacion4.csv', 'rt') as f4:
+        text4 = f4.readlines()
+text4 [:5]
+
+#Decimales 
+text1 = [line.replace(',', '.') for line in text1]
+text2 = [line.replace(',', '.') for line in text2]
+text3 = [line.replace(',', '.') for line in text3]
+text4 = [line.replace(',', '.') for line in text4]
+
+#se elimina el retorno de carro '\n'
+text1 = [line[:-1] for line in text1]
+text2 = [line[:-1] for line in text2]
+text3 = [line[:-1] for line in text3]
+text4 = [line[:-1] for line in text4]
+
+
+## Se separan los campos deliminados por comas
+text1 = [line.replace(';', ',') for line in text1]
+text2 = [line.replace(';', ',') for line in text2]
+text3 = [line.replace(';', ',') for line in text3]
+text4 = [line.replace(';', ',') for line in text4]
+
+
